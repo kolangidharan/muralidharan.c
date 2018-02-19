9507e239c67e56afef577881baeb8e60a279cf91
kolangidharan <35991774+kolangidharan@users.noreply.github.com>
2018-02-19 13:55:24 +0530

    Create covai.c

---
+++ covai.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+	char a[1000];
+	int n,i,count=0;
+	scanf("%[^\t\n]s",a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+	   if(a[i]>='0'&&a[i]<='9')
+	    {
+	        count++;
+	    }
+	    else
+	    {
+	        continue;
+	    }
+	}
+	printf("%d",count);
+getch();
+}
