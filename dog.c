e5d067eb468af16dd8ee0dbd33203bc473c743b2
kolangidharan <35991774+kolangidharan@users.noreply.github.com>
2018-02-21 14:11:21 +0530

    Create dog.c

---
+++ dog.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[25],b[40];
+printf("Enter the string");
+scanf("%s",a);
+printf("Enter the string");
+scanf("%s",b);
+printf("The string is....: %s%s",a,b);
+getch();
+}
