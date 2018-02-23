6828b647d81368c2a67a01b6afa1de6a485e1b97
kolangidharan <35991774+kolangidharan@users.noreply.github.com>
2018-02-23 16:04:23 +0530

    Create dheeraj.c

---
+++ dheeraj.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    n=n-1;
+    if(n%2==0)
+    {
+    	printf("%d",n);
+	}
+	else
+	{
+		n=n+1;
+		printf("%d",n);
+	}
+
+    return 0;
+
+}
+getch();
+}
