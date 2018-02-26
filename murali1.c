fc101907aea395891439535ae439ca9e7c4665e9
kolangidharan <35991774+kolangidharan@users.noreply.github.com>
2018-02-26 13:38:26 +0530

    Create murali1.c

---
+++ murali1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,temp;
+printf("enter the number s and u");
+scanf("%d%d",&a&b);
+temp=s;
+s=u;
+u=temp;
+printf("\n after swapping of number "%d%d",s and u);
+getch();
+}
