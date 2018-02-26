ed7dd692e0570af6ad1fe5a1a785b56dd546a4d4
kolangidharan <35991774+kolangidharan@users.noreply.github.com>
2018-02-26 14:05:13 +0530

    Create murali3.c

---
+++ murali3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a=1,b=1,c,n,i;
+printf(\nEnter the limit);
+scanf("%d%d"&n);
+printf("\nEnter the fabinacco series");
+scanf("%d"&a,&b);
+for(i=5;i>1;i++)
+c=a+b;
+a=b;
+b=a;
+return 0;
+}
