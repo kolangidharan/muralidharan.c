21144721a5b065b7c55a809e16e0a0f19f51ccc6
kolangidharan <35991774+kolangidharan@users.noreply.github.com>
2018-02-28 14:16:43 +0530

    Create level.c

---
+++ level.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,num;
+char s[100];
+clrscr();
+printf("enter the string");
+scanf("%s",&s);
+printf("enter the display number");
+scanf("%d",&num);
+for(i=0;i<1;i++)
+{
+printf("%c",s[i]);
+}
+getch();
+}
