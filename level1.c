5246ea611d65e1f394e97abb9403f00a845638f0
kolangidharan <35991774+kolangidharan@users.noreply.github.com>
2018-02-28 14:33:11 +0530

    Create level1.c

---
+++ level1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+	char a[100];
+	gets(a);
+	int i,count=1;
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]==' ')
+		{
+			count=count+1;
+		}
+	}
+	printf("%d",count);
+  getch();
+}
