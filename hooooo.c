d5d651272117b24eb9fd823bb7d7eb9dbc8271eb
kolangidharan <35991774+kolangidharan@users.noreply.github.com>
2018-03-05 13:36:33 +0530

    Rename gooooo.c to hooooo.c

---
+++ hooooo.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int r,v,s;
+printf("enter the numbers");
+scanf("%d%d",&r,&v);
+s=r-v;
+if(s%2==1)
+{
+printf("even");
+}
+else
+{
+printf("no');
+}
+getch();
+}
