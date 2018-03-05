ece9c87e8227cb95c3253fea0bffc642b6481129
kolangidharan <35991774+kolangidharan@users.noreply.github.com>
2018-03-05 13:40:45 +0530

    Rename baby1.c to baby0.c

---
+++ baby0.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main() 
+{
+int n,s,t,u;
+printf("Enter a number:");
+scanf("%d",&n);
+s=n/2;
+if(s%2==0)
+{
+u=2*s;
+printf("\n The nearest power is:%d",u);
+}
+else
+{
+t=s-1;
+u=2*t;
+printf("\n The nearest power is:%d",u);
+getch();
+}
+}
