2405dd829d3acd5599a1c6119e3509e5cb813a50
kolangidharan <35991774+kolangidharan@users.noreply.github.com>
2018-03-05 13:30:47 +0530

    Create level98.c

---
+++ level98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+	int v;
+	printf("enter the number");
+	scanf("%d",&v);
+	if(v%7==0)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+    getch();
+	}
+}
