cf4ea6e068a8fd76e83fa64490aed4029037704b
kolangidharan <35991774+kolangidharan@users.noreply.github.com>
2018-03-05 13:24:05 +0530

    Create level100.c

---
+++ level100.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,f=0;
+printf("enter the number");
+scanf("%d",&n);
+for(i=2;i<=n/2;i++)
+{
+if(n/i==0)
+{
+f=1;
+}
+if(f==0)
+{
+prinf("%d is a prime number");
+}
+else
+{
+printf("%d is not a prime number");
+getch();
+}
+}
+}
