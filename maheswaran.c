e160d794f1536f74535fa08b994e0afb3c7b3cd2
kolangidharan <35991774+kolangidharan@users.noreply.github.com>
2018-03-07 14:01:04 +0530

    Create maheswaran.c

---
+++ maheswaran.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main();
+{
+int c,d=0;
+char s;
+printf("enter the word");
+scanf("%s",s);
+c=strlen(s);
+for(i=1;i<c;i++)
+{
+if(s='A'||s='E'||s='I'||s='O'||s='U'||s='a'||s='e'||s='i'||s='o'||s='u')
+{
+d=d+1;
+}
+}
+printf("The given string vowels are..%d",d);
+getch();
+}
