bef8e84fdfb61e5d2de169e6deb152bfd45f7f79
kolangidharan <35991774+kolangidharan@users.noreply.github.com>
2018-03-14 15:47:08 +0530

    Create muralidharan140318.c

---
+++ muralidharan140318.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+int u;
+printf("Enter a number:");
+scanf("%d",&u);
+if(s%13==0)
+{
+printf("YES");
+}
+else
+{
+printf("NO");
+}
+getch();
+}
